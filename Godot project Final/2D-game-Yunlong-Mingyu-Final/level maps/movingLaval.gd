45f6a60d9bc04df1b1dc104dec0a27629d7821bd
Jerryvsjojo <159824637+Jerryvsjojo@users.noreply.github.com>
2024-04-27 15:38:42 -0400

    add movingLava

---
+++ Godot project Final/2D-game-Yunlong-Mingyu-Final/level maps/movingLaval.gd
@@ -0,0 +1,17 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y -=1
+
+
+func _on_moving_lava_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.has_method("kill"):
+		body.kill()
+		get_tree().change_scene_to_file("res://texts/death scenes/burning to death.tscn") 
--- Godot project Final/2D-game-Yunlong-Mingyu-Final/texts/death scenes/Button.gd
+++ Godot project Final/2D-game-Yunlong-Mingyu-Final/texts/death scenes/Button.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://level maps/level_1.tscn") 
+	get_tree().change_scene_to_file("res://level maps/realLevelTwo.tscn") 
