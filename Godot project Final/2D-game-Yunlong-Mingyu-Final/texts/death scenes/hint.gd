9aa320629679f256ff6eb1823106de520db0ef48
Jerryvsjojo <159824637+Jerryvsjojo@users.noreply.github.com>
2024-04-28 17:53:34 -0400

    add hint to death scene

---
+++ Godot project Final/2D-game-Yunlong-Mingyu-Final/texts/death scenes/hint.gd
@@ -0,0 +1,19 @@
+extends Label
+var sentences = [
+	"Don't die so easily, please.",
+	"Certainly this is not your problem; it is not RTX 4090",
+	"Beware of the lava and meteorite.",
+	"Try to jump first and then move left and right.",
+	"My game would crash if you die too many times.",
+	"The dragon seems don't like the taste of men.",
+	"Practice some Dark Soul Games before you try ours.",
+	"Timing is important, don't just press keys."
+]
+
+func _ready():
+	randomize()
+	sentences.shuffle()
+	text = sentences[0]
+	print(sentences[0])
+  
+
